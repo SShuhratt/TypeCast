8f03142d1c4402066ece785b75bdc4bc78e7ec9d
Shuhrat Odilbekov <shuhratodilbekov3@gmail.com>
2026-09-13 00:19:22 +0500

    fix: resolve Buffer and stream browser injection in doc_extractor bundle

--- frontend/js/libs/stream-mock.js
+++ frontend/js/libs/stream-mock.js
@@ -1,2 +1,48 @@
-class Readable {}
+const EventEmitter = require('events');
+
+class Readable extends EventEmitter {
+  constructor() {
+    super();
+    this._started = false;
+  }
+
+  push(chunk) {
+    if (chunk === null) {
+      queueMicrotask(() => {
+        this.emit('end');
+      });
+      return false;
+    }
+    queueMicrotask(() => {
+      this.emit('data', chunk);
+    });
+    return true;
+  }
+
+  on(event, listener) {
+    super.on(event, listener);
+    if (event === 'data' && !this._started) {
+      this._started = true;
+      queueMicrotask(() => this._runLoop());
+    }
+    return this;
+  }
+
+  async _runLoop() {
+    while (!this._done) {
+      try {
+        const ret = this._read();
+        if (ret && typeof ret.then === 'function') {
+          await ret;
+        }
+      } catch (err) {
+        this.emit('error', err);
+        break;
+      }
+    }
+  }
+
+  _read() {}
+}
+
 module.exports = { Readable };
